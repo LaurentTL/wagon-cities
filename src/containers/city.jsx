edea6249f81f47f4c1d540a7719e2761daf8673b
Laurent Treuil <laurent.treuil@gmail.com>
2020-08-06 17:06:22 +0200

    citylist and city display

---
+++ src/containers/city.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class City extends Component {
+  render() {
+    return (
+      <div className="active-city">
+        <h1>todo</h1>
+      </div>
+    );
+  }
+}
+
+export default City;
---
+++ src/containers/city_list.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CityList extends Component {
+  render() {
+    return (
+      <div className="cities">
+        <h1>todo</h1>
+      </div>
+    );
+  }
+}
+
+export default CityList;
