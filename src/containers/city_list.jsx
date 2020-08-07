3da76877065aef68663b72948a2ba0900d4daf1b
Laurent Treuil <laurent.treuil@gmail.com>
2020-08-07 16:43:06 +0200

    syntax error on render list, fixed

--- src/containers/city_list.jsx
+++ src/containers/city_list.jsx
@@ -1,20 +1,11 @@
 import React, { Component } from 'react';
 import City from '../containers/city';
 
-
 class CityList extends Component {
-  static defaultProps = {
-    cities: [
-    { name: 'Paris', address: '16 Villa Gaudelet, 75011 Paris', slug: 'paris' },
-    { name: 'London', address: '14-22 Elder St, London E1 6BT', slug: 'london' },
-    { name: 'Berlin', address: 'Rudi-Dutschke-Straße 26, 10969 Berlin', slug: 'berlin' },
-    ]
-  }
-
-  renderList = (props) => {
-    return (
-      this.props.map((city) => <City key={city.name} city={city} />)
-    );
+  renderList() {
+    return this.props.map((city) => {
+      return <City key={city.name} city={city} />;
+    });
   }
 
   render() {
